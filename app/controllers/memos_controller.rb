ddc96ddb973b451580c757ea72ce99ec8a108ca2
taka-0713 <ottake07@gmail.com>
2021-01-17 16:34:30 +0900

    新規メモがmemosテーブルに保存されるよう実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -4,9 +4,22 @@ class MemosController < ApplicationController
   end
 
   def new
+    @memo = Memo.new
   end
 
   def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:movie_title, :director, :music, :movie_genre_id, :music_genre_id, :theme_song, :inserted_song, :text, :image).merge(user_id: current_user.id)
   end
 
 end

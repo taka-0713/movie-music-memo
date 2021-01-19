3893d44b6cf79ac529fc898563cbd665e555a8c0
taka-0713 <ottake07@gmail.com>
2021-01-19 13:58:08 +0900

    元の情報を引き継いで編集ページに遷移&編集機能を実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -21,6 +21,22 @@ class MemosController < ApplicationController
     @memo = Memo.find(params[:id])
   end
 
+  def edit
+    @memo = Memo.find(params[:id])
+    unless @memo.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @memo = Memo.find(params[:id])
+    if @memo.update(memo_params)
+      redirect_to memo_path(@memo.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     memo = Memo.find(params[:id])
     memo.destroy
